fd73e166883d3a43341b9a2d9dc59be1c8c6e68c
Nurullah Taşkan <nttaskan@gmail.com>
2023-02-19 00:45:27 +0300

    18/02/2023

--- src/test/java/techproed/tests/smoketests/Day22_PositiveLoginTest.java
+++ src/test/java/techproed/tests/smoketests/Day22_PositiveLoginTest.java
@@ -1,6 +1,7 @@
 package techproed.tests.smoketests;
 
 import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 import techproed.pages.BlueRentalHomePage;
 import techproed.pages.BlueRentalLoginPage;
@@ -28,17 +29,17 @@ public class Day22_PositiveLoginTest {
 
         blueRentalHomePage = new BlueRentalHomePage();
         blueRentalLoginPage = new BlueRentalLoginPage();
-
+        Reporter.log("SAYFAYA GİT");
         Driver.getDriver().get(ConfigReader.getProperty("app_url"));
-
+        Reporter.log("LOGİN BUTONUNA TIKLA");
         blueRentalHomePage.loginLink.click();
-
+        Reporter.log("GİRİŞ BİLGİLERİNİ GİR");
         blueRentalLoginPage.emailBox.sendKeys(ConfigReader.getProperty("admin_email"));
         blueRentalLoginPage.passwordBox.sendKeys(ConfigReader.getProperty("admin_sifre"));
         blueRentalLoginPage.loginButton.click();
-
+        Reporter.log("GİRİŞ YAPILDIĞINI DOĞRULA");
         Assert.assertTrue(blueRentalHomePage.userID.isDisplayed());
-
+        Reporter.log("DRİVERİ KAPAT");
         Driver.closeDriver();
 
     }
